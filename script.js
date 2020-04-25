69c0235a5041ce1e8b95c53898692dd74b3ee6f7
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-04-25 17:14:08 +0500

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const rulesBtn = document.getElementById('rules-btn');
+const closeBtn = document.getElementById('close-btn');
+const rules = document.getElementById('rules');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let score = 0;
+
+const brickRowCount = 9;
+const brickColumnCount = 5;
