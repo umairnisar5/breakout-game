57a47fbca6ac3b84c02de41485cb99e47102efdc
umairnisar5 <49435408+umairnisar5@users.noreply.github.com>
2020-04-25 17:15:24 +0500

    add rule

--- script.js
+++ script.js
@@ -8,3 +8,7 @@ let score = 0;
 
 const brickRowCount = 9;
 const brickColumnCount = 5;
+
+// Rules and close event handlers
+rulesBtn.addEventListener('click', () => rules.classList.add('show'));
+closeBtn.addEventListener('click', () => rules.classList.remove('show'));
